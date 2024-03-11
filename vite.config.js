368aa1d5604b253088bfaba91c94d957cf7357f6
aatrishin <aatrishin@domclick.ru>
2024-03-11 09:35:25 +0300

    sprint_1: add multiple pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,16 @@ import handlebars from 'vite-plugin-handlebars';
 export default defineConfig({
     root: resolve(__dirname, 'src'),
     build: {
-        outDir: resolve(__dirname, 'dist')
+        outDir: resolve(__dirname, 'dist'),
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                auth: resolve(__dirname, 'src/pages/Authorization/index.html'),
+                settings: resolve(__dirname, 'src/pages/Settings/index.html'),
+                profile: resolve(__dirname, 'src/pages/Profile/index.html'),
+                registration: resolve(__dirname, 'src/pages/Registration/index.html')
+            }
+        }
     },
     plugins: [handlebars({
         partialDirectory: resolve(__dirname, 'src/partials'),
