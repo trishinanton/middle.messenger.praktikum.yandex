e5a61da13cd60b732b3e93f50a75c4f2442c2c71
aatrishin <aatrishin@domclick.ru>
2024-05-27 09:54:05 +0300

    change husky

--- mochaSetup.js
+++ mochaSetup.js
@@ -1,8 +1,8 @@
-import {JSDOM} from 'jsdom'
+import { JSDOM } from 'jsdom';
 
-const jsdom = new JSDOM('<body></body>', {})
+const jsdom = new JSDOM('<body></body>', {});
 
-global.window = jsdom.window
-global.XMLHttpRequest = jsdom.window.XMLHttpRequest
-global.DOMParser = jsdom.window.DOMParser
-global.document = jsdom.window.document
+global.window = jsdom.window;
+global.XMLHttpRequest = jsdom.window.XMLHttpRequest;
+global.DOMParser = jsdom.window.DOMParser;
+global.document = jsdom.window.document;
